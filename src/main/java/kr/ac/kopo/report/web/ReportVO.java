a13d8d83367c6af76cc7fca8580c777c4e48de96
tech.ykoh@gmail.com <tech.ykoh@gmail.com>
2023-09-23 03:20:43 +0900

    ReportController test

---
+++ src/main/java/kr/ac/kopo/report/web/ReportVO.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.report.web;
+
+public class ReportVO {
+	private int reportNo;
+	private int roomNo;
+	private int userNo;
+	private String reportCheck1;
+	private String reportCheck2;
+	private String reportCheck3;
+	private String reportContent;
+	private String userAt;
+	private String agreeAt;
+}

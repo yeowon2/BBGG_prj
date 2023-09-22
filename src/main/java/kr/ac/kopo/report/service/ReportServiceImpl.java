fa566823150eee8238a6b7e6806709650aa98794
oyk0425 <oyk0425@hotmail.com>
2023-09-23 02:04:40 +0900

    report controller test

---
+++ src/main/java/kr/ac/kopo/report/service/ReportServiceImpl.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.report.service;
+
+import java.util.List;
+
+import kr.ac.kopo.report.web.ReportVO;
+
+public class ReportServiceImpl implements ReportService {
+
+	@Override
+	public List<ReportVO> reportList(ReportVO reportVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
